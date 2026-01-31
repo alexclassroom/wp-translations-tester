f2aafb80f57436bcb10e8e05251efe19d0734b63
Sadler Jerome <jerome@sadler-jerome.fr>
2026-01-31 10:29:54 +0100

    Add 'SEO Settings' string for translation

--- wp-translations-tester/admin.php
+++ wp-translations-tester/admin.php
@@ -41,6 +41,7 @@ function wpt_tester_admin_page_output() {
 	  <p><?php esc_html_e( 'Storage', 'wp-translations-tester' ); ?></p>
 	  <p><?php esc_html_e( 'Forms', 'wp-translations-tester' ); ?></p>
 	  <p><?php esc_html_e( 'Edit Form', 'wp-translations-tester' ); ?></p>
+	  <p><?php esc_html_e( 'SEO Settings', 'wp-translations-tester' ); ?></p>
    <p><?php esc_html_e(
 					sprintf(
 						/* translators: %d is a number of strings translations */
